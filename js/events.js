b50bd0feba34533c9300b39a8e3a873c9cf9ec53
Cecelia Smith <ceceliacreates@gmail.com>
2019-03-03 20:10:38 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,13 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  })
+}
 
+function frameIt() {
+  $(document).on("load",)
+}
 $(document).ready(function(){
 
 // call functions here
